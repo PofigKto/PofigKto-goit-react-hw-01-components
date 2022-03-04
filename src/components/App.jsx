b8f53a7b27be1b5fc91379c0577f48c9281ce10e
Luydmyla <lytic.ok@gmail.com>
2022-03-05 00:13:24 +0200

    update task1
    -added styles

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,37 +1,29 @@
 import user from '../data/user.json'
 import statistics from '../data/statistics.json';
-import Profile from './profile/Profile';
+import Profile from './Profile/Profile';
 import StatisticList from './statistics/StatisticsList';
 import Section from './statistics/Section';
 import friends from '../data/friends.json';
-    import FriendList from './friend-list/FriendList';
+import FriendList from './friend-list/FriendList';
+import Container from './Container/Container';
 
 
 export default function App() {
   return (
-    <div
-      // style={{
-      //   height: '100vh',
-      //   display: 'flex',
-      //   justifyContent: 'center',
-      //   alignItems: 'center',
-      //   fontSize: 40,
-      //   textTransform: 'uppercase',
-      //   color: '#010101',
-      // }}
-    >
+    <Container>
      <Profile
-  username={user.username}
-  tag={user.tag}
-  location={user.location}
-  avatar={user.avatar}
-  stats={user.stats}
+       username={user.username}
+       tag={user.tag}
+       location={user.location}
+       avatar={user.avatar}
+       stats={user.stats}
       />
-<Section title="Upload stats" stats={statistics} />;
-      <StatisticList stats={statistics} />;
-  
-
-<FriendList friends={friends} />,
-    </div>
+      <Section title="Upload stats" stats={statistics} />
+      <StatisticList stats={statistics} />
+      <FriendList friends={friends} />
+          </Container>
+     
+    
+     
   );
 };
--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -1,76 +1,27 @@
 import PropTypes from 'prop-types'; // ES6
 // import defaultImage from './default.jpg';
+import s from './Profile.module.css';
 const Profile = ({ username, tag, location, avatar, stats }) => {
   return (
-    <div
-      className="profile"
-      style={{
-        color: 'red',
-      }}
-    >
-      <div
-        className="description"
-        style={{
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'center',
-          alignContent: 'center',
-          alignItems: 'center',
-          backgroundColor: 'aqua',
-        }}
-      >
-        <img
-          src={avatar}
-          alt="User avatar"
-          className="avatar"
-          style={{
-            display: 'block',
-            backgroundColor: 'purple',
-            borderRadius: '50%',
-          }}
-        />
-        <p
-          className="name"
-          style={{
-            color: 'green',
-          }}
-        >
-          {username}
-        </p>
-        <p
-          className="tag"
-          style={{
-            color: 'green',
-          }}
-        >
-          {tag}
-        </p>
-        <p
-          className="location"
-          style={{
-            color: 'green',
-          }}
-        >
-          {location}
-        </p>
+    <div className={s.container}>
+      <div className={s.description}>
+        <img src={avatar} alt="User avatar" className={s.avatar} />
+        <p className={s.name}>{username}</p>
+        <p className={s.tag}>{tag}</p>
+        <p className={s.location}>{location}</p>
       </div>
-      <ul
-        className="stats"
-        style={{
-          color: 'blue',
-        }}
-      >
-        <li>
-          <span className="label">Followers : </span>
-          <span className="quantity">{stats.followers}</span>
+      <ul className={s.stats}>
+        <li className={s.item}>
+          <span className={s.label}>Followers </span>
+          <span className={s.quantity}>{stats.followers}</span>
         </li>
-        <li>
-          <span className="label">Views : </span>
-          <span className="quantity">{stats.views}</span>
+        <li className={s.item}>
+          <span className={s.label}>Views </span>
+          <span className={s.quantity}>{stats.views}</span>
         </li>
-        <li>
-          <span className="label">Likes : </span>
-          <span className="quantity">{stats.likes}</span>
+        <li className={s.item}>
+          <span className={s.label}>Likes </span>
+          <span className={s.quantity}>{stats.likes}</span>
         </li>
       </ul>
     </div>
